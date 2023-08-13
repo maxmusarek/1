5e2ad1dbc80872edc66c940bf9b0b739935d81a0
Maksym Mazurek <maxmusarek@gmail.com>
2023-08-13 16:55:17 +0300

    ghhgfghf

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("\n hello java");
+    char x='\uFF01';
+        float fl = 30.6f;
+        String text = "\n\tВот мысль, которой весь я предан\n"+
+                "Итог всего, что ум скопил.\n"+
+                "Лишь тот, кем бой за жизнь изведан,\n"+
+                "Жизнь и свободу заслужил.\n";
+
+        System.out.println(text);
+        System.out.println("\n \t hello java fuck you"+x+fl+text);
+    }
+}
